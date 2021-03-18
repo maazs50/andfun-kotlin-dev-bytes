f545f83bcb2d79b03237347939a7bd1d35eea4e7
apple <apple@apples-MacBook-Pro.local>
2021-03-18 20:10:17 +0530

    Caching done

---
+++ app/src/main/java/com/example/android/devbyteviewer/work/RefreshDataWork.kt
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import androidx.work.impl.model.WorkName
+import com.example.android.devbyteviewer.database.getDatabase
+import com.example.android.devbyteviewer.repository.VideosRepository
+import retrofit2.HttpException
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters):
+        CoroutineWorker(appContext,params){
+    companion object{
+        const val Work_NAME = "RefreshDataWorker"
+    }
+    override suspend fun doWork():Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        repository.refreshVideos()
+
+        return try {
+            repository.refreshVideos()
+            return Result.success()
+        }catch (exception: HttpException){
+            return Result.failure()
+        }
+    }
+
+
+}
